09b2b534eaa170db245ceffdd19fe87c9759e3e1
Dominik Najberg <dominik.najberg@gmail.com>
2019-06-02 21:26:32 +0200

    BubbleSorter fewer comparisons with every loop

--- src/Application/Sort/BubbleSort.php
+++ src/Application/Sort/BubbleSort.php
@@ -11,11 +11,16 @@ class BubbleSort implements SortInterface
         $arrayLength = count($sortable);
 
         for ($i = 0; $i < $arrayLength; $i++) {
-            for ($j = 0; $j < $arrayLength - 1; $j++) {
+            $swapped = false;
+            for ($j = 0; $j < $arrayLength - $i - 1; $j++) {
                 if ($sortable[$j] > $sortable[$j + 1]) {
                     // PHP > 7.1 swap array elements
                     [$sortable[$j], $sortable[$j + 1]] = [$sortable[$j + 1], $sortable[$j]];
+                    $swapped = true;
                 }
+            }
+            if (false === $swapped) {
+                break;
             }
         }
     }
